53fd410925346e9b0afdcca0bfa198c782a1eeea
REDfriend <tongtongkonthai.34930@gmail.com>
2023-08-11 23:00:06 +0700

    Update PID.h

--- Handle/PID.h
+++ Handle/PID.h
@@ -2,8 +2,24 @@
 #define PID_h
 
 #include <Arduino.h>
+#ifndef motor_h
+#define motor_h
 
-int[] sensorsValue = new int[5]; // do not use 0 index
+
+const int BZ = 4;
+
+// ตั้งค่าขาที่ใช้ในการควบคุม L298P Shield
+const int M1 = 10;  // ขาที่ใช้ในการควบคุมความเร็วมอเตอร์ A
+const int MA1 = 12; // ให้มอเตอร์หมุนไปด้านหน้า
+const int MA2 = 3;  // ให้มอเตอร์หมุนไปด้านหลัง
+
+const int M2 = 11;  // ขาที่ใช้ในการควบคุมความเร็วมอเตอร์ B
+const int MB1 = 13; // ให้มอเตอร์หมุนไปด้านหน้า
+const int MB2 = 8;  // ให้มอเตอร์หมุนไปด้านหลัง
+
+
+
+int[] sensorsValue = new int[7]; // do not use 0 index
 
 int lineV = 0;
 int groundV = 0;
@@ -25,8 +41,20 @@ int Kp = 1.0;
 int Kd = 0.03;
 int Ki = 0.1;
 
+void motor(int PWA, int PWB, int MAA1, int MAA2, int MBB1, int MBB2)
+{
+  analogWrite(M1, PWA);
+  analogWrite(M2, PWB);
+
+  digitalWrite(MA1, MAA1);
+  digitalWrite(MB1, MBB1);
+  digitalWrite(MA2, MAA2);
+  digitalWrite(MB2, MBB2);
+}
+
+
 bool B(int n){  
-  if(n < meanV){ // is black
+  if(n > 980){ // is black
       return true;
   }else{
       return false;
@@ -34,7 +62,7 @@ bool B(int n){
 }
 
 bool W(int n){  
-    if(n >= meanV){ // is white
+    if(n <= 979){ // is white
       return true;
   }else{
       return false;
@@ -47,25 +75,42 @@ void Tl(){
         sensorsValue[i] = analog(i);
     }
    
-    if( W(sensorsValue[0]) && W(sensorsValue[1]) && W(sensorsValue[2]) && W(sensorsValue[3]) && B(sensorsValue[4]) ){ 
+    if( W(sensorsValue[0]) && W(sensorsValue[1]) && W(sensorsValue[2]) && W(sensorsValue[3]) && W(sensorsValue[4]) &&W(sensorsValue[5]) &&W(sensorsValue[6]) &&B(sensorsValue[7]) ){ 
         error = 4;
-    }else if( W(sensorsValue[0]) && W(sensorsValue[1]) && W(sensorsValue[2]) && B(sensorsValue[3]) && B(sensorsValue[4]) ){
+    }else if( W(sensorsValue[0]) && W(sensorsValue[1]) && W(sensorsValue[2]) && W(sensorsValue[3]) && W(sensorsValue[4]) &&W(sensorsValue[5]) &&B(sensorsValue[6]) &&B(sensorsValue[7]) ){
+      error = 4;
+    }else if( W(sensorsValue[0]) && W(sensorsValue[1]) && W(sensorsValue[2]) && W(sensorsValue[3]) && W(sensorsValue[4]) &&B(sensorsValue[5]) &&B(sensorsValue[6]) &&B(sensorsValue[7])){
       error = 3;
-    }else if( W(sensorsValue[0]) && W(sensorsValue[1]) && W(sensorsValue[2]) && B(sensorsValue[3]) && W(sensorsValue[4]) ){
+    }else if( W(sensorsValue[0]) && W(sensorsValue[1]) && W(sensorsValue[2]) && W(sensorsValue[3]) && B(sensorsValue[4]) &&B(sensorsValue[5]) &&B(sensorsValue[6]) &&B(sensorsValue[7])) {
+      error = 3;
+    }else if( W(sensorsValue[0]) && W(sensorsValue[1]) && W(sensorsValue[2]) && B(sensorsValue[3]) && B(sensorsValue[4]) &&B(sensorsValue[5]) &&B(sensorsValue[6]) &&B(sensorsValue[7])) {
       error = 2;
-    }else if( W(sensorsValue[0]) && W(sensorsValue[1]) && B(sensorsValue[2]) && B(sensorsValue[3]) && W(sensorsValue[4]) ){
+    }else if( W(sensorsValue[0]) && W(sensorsValue[1]) && B(sensorsValue[2]) && B(sensorsValue[3]) && B(sensorsValue[4]) &&B(sensorsValue[5]) &&B(sensorsValue[6]) &&B(sensorsValue[7])){
+      error = 2;
+    }else if( W(sensorsValue[0]) && B(sensorsValue[1]) && B(sensorsValue[2]) && B(sensorsValue[3]) && B(sensorsValue[4]) &&B(sensorsValue[5]) &&B(sensorsValue[6]) &&B(sensorsValue[7])){
       error = 1;
-    }else if( W(sensorsValue[0]) && W(sensorsValue[1]) && B(sensorsValue[2]) && W(sensorsValue[3]) && W(sensorsValue[4]) ){
+    }else if( B(sensorsValue[0]) && B(sensorsValue[1]) && B(sensorsValue[2]) && B(sensorsValue[3]) && B(sensorsValue[4]) &&B(sensorsValue[5]) &&B(sensorsValue[6]) &&B(sensorsValue[7])){
       error = 0;
-    }else if( W(sensorsValue[0]) && B(sensorsValue[1]) && B(sensorsValue[2]) && W(sensorsValue[3]) && W(sensorsValue[4]) ){
-      error = -1;
-    }else if( W(sensorsValue[0]) && B(sensorsValue[1]) && W(sensorsValue[2]) && W(sensorsValue[3]) && W(sensorsValue[4]) ){
-      error = -2;
-    }else if( B(sensorsValue[0]) && B(sensorsValue[1]) && W(sensorsValue[2]) && W(sensorsValue[3]) && W(sensorsValue[4]) ){
+    }else if( W(sensorsValue[0]) && B(sensorsValue[1]) && B(sensorsValue[2]) && B(sensorsValue[3]) && B(sensorsValue[4]) &&B(sensorsValue[5]) &&B(sensorsValue[6]) &&W(sensorsValue[7])){
+      error = 0;
+    }else if( W(sensorsValue[0]) && W(sensorsValue[1]) && B(sensorsValue[2]) && B(sensorsValue[3]) && B(sensorsValue[4]) &&B(sensorsValue[5]) &&W(sensorsValue[6]) &&W(sensorsValue[7])){
+      error = 0;
+    }else if( W(sensorsValue[0]) && W(sensorsValue[1]) && W(sensorsValue[2]) && B(sensorsValue[3]) && B(sensorsValue[4]) &&BWsensorsValue[5]) &&W(sensorsValue[6]) &&W(sensorsValue[7])){
+      error = 0;
+    }else if( B(sensorsValue[0]) && W(sensorsValue[1]) && W(sensorsValue[2]) && W(sensorsValue[3]) && W(sensorsValue[4]) &&W(sensorsValue[5]) &&W(sensorsValue[6]) &&W(sensorsValue[7]) ){
+      error = -4;
+    }else if( B(sensorsValue[0]) && B(sensorsValue[1]) && W(sensorsValue[2]) && W(sensorsValue[3]) && W(sensorsValue[4]) &&W(sensorsValue[5]) &&W(sensorsValue[6]) &&W(sensorsValue[7]) ){
+      error = -4;
+    }else if( B(sensorsValue[0]) && B(sensorsValue[1]) && B(sensorsValue[2]) && W(sensorsValue[3]) && W(sensorsValue[4]) &&W(sensorsValue[5]) &&W(sensorsValue[6]) &&W(sensorsValue[7]) ){
       error = -3;
-    }else if( B(sensorsValue[0]) && W(sensorsValue[1]) && W(sensorsValue[2]) && W(sensorsValue[3]) && W(sensorsValue[4]) ){
-        error = -4;
-    }
+    }else if( B(sensorsValue[0]) && B(sensorsValue[1]) && B(sensorsValue[2]) && B(sensorsValue[3]) && W(sensorsValue[4]) &&W(sensorsValue[5]) &&W(sensorsValue[6]) &&W(sensorsValue[7]) ){
+      error = -3;
+    }else if( B(sensorsValue[0]) && B(sensorsValue[1]) && W(sensorsValue[2]) && B(sensorsValue[3]) && B(sensorsValue[4]) &&W(sensorsValue[5]) &&W(sensorsValue[6]) &&W(sensorsValue[7]) ){
+      error = -2;
+    }else if( B(sensorsValue[0]) && B(sensorsValue[1]) && W(sensorsValue[2]) && B(sensorsValue[3]) && B(sensorsValue[4]) &&B(sensorsValue[5]) &&W(sensorsValue[6]) &&W(sensorsValue[7]) ){
+      error = -2;
+    }else if( B(sensorsValue[0]) && B(sensorsValue[1]) && W(sensorsValue[2]) && B(sensorsValue[3]) && B(sensorsValue[4]) &&B(sensorsValue[5]) &&B(sensorsValue[6]) &&W(sensorsValue[7]) ){
+      error = -1;
     /// check WWWWW
     else if( W(sensorsValue[0]) && W(sensorsValue[1]) && W(sensorsValue[2]) && W(sensorsValue[3]) && W(sensorsValue[4]) ){
         error = pre_error;
@@ -83,9 +128,8 @@ void Tl(){
    if(leftSpeed < -maxSpeed) leftSpeed = -maxSpeed;
    if(rightSpeed < -maxSpeed) rightSpeed = -maxSpeed;
    
-   motor(1,leftSpeed);
-   motor(2,rightSpeed);
-   sleep(33);  
+   motor(leftSpeed,rightSpeed,1,0,1,0);
+  
    
    pre_error = error;
    sum_error += error;
